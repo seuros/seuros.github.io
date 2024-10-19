7911e02c011b878e4c5134130fb9f9ab7416000b
Abdelkader Boudih <terminale@gmail.com>
2024-10-19 14:15:02 +0100

    add deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,26 @@
 import { defineConfig, passthroughImageService } from "astro/config";
 import mdx from "@astrojs/mdx";
-
 import sitemap from "@astrojs/sitemap";
-
 import cloudflare from "@astrojs/cloudflare";
+import tailwind from "@astrojs/tailwind";
+
+
+const baseConfig = {
+  site: "https://seuros.com",
+  integrations: [mdx(), sitemap(), tailwind()],
+  image: { service: passthroughImageService() },
+};
+
+const config = process.env.CI
+  ? {
+      ...baseConfig,
+      output: "dist",
+    }
+  : {
+      ...baseConfig,
+      adapter: cloudflare({ imageService: "cloudflare" }),
+      output: "server",
+    };
 
 // https://astro.build/config
-export default defineConfig({
-  site: "https://seuros.com",
-  integrations: [mdx(), sitemap()],
-  adapter: cloudflare({ imageService: "cloudflare" }),
-  image: { service: passthroughImageService() },
-  output: "server",
-});
+export default defineConfig(config);
